d8dd291734adc7722e18262de32a7ac7c9bcd512
zaife <2020813046@student.uitm.edu.my>
2022-06-21 02:33:31 +0800

    Adding lab 5 Client code python

--- childtask.c
+++ childtask.c
@@ -1,28 +1,32 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<unistd.h>
-#include<sys/wait.h>
-
-void childTask(){
- printf("Salam, saya anak tau\n");
-}
-void parentTask(){
- printf("Dan saya adalah bapak");
-}
-int main(void){
- pid_t pid = fork();
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
  
- if(pid=0){
-  childTask();
-  exit(EXIT_SUCCESS);
- }
- else if(pid>0){
-  wait(NULL);
-  parentTask();
- }
- else{
-  printf("Unable to create child process.");
- }
- return EXIT_SUCCESS;
+void childTask() {
+  printf("Salam, saya anak tau\n");
 }
+ 
+void parentTask() {
+  printf("Dan saya adalah bapaknya n");
+}
+ 
+int main(void) {
+  pid_t pid = fork();
+ 
+  if(pid == 0) {
+    childTask();
+    exit(EXIT_SUCCESS);
+  }
+  else if(pid > 0) {
+    wait(NULL);
+    parentTask();
+  }
+  else {
+    printf("Unable to create child process.");
+  }
+ 
+  return EXIT_SUCCESS;
+}
+
